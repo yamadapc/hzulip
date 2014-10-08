72f2ec71189b265ac051d5b5041b5b42128e2b70
yamadapc <tacla.yamada@gmail.com>
2014-10-08 17:39:43 -0400

    Refactor reorganize and add haddock documentation
    This also makes a small change to the exposed API, both as far as what
    functions are exposed and the actual API - `newZulip` had a small
    modification to its arguments.

--- src/HZulip.hs
+++ src/HZulip.hs
@@ -1,5 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
-module HZulip where
+module HZulip ( Message(..)
+              , ZulipClient(..)
+              , defaultBaseUrl
+              , newZulip
+              , sendMessage
+              , wasSent
+              )
+  where
 
 import Control.Lens
 import Control.Monad
@@ -8,11 +15,21 @@ import qualified Data.Text as DT (unpack)
 import qualified Data.ByteString.Char8 as BS (pack)
 import Network.Wreq
 
+-- Public types and functions:
+-------------------------------------------------------------------------------
+
+-- |Represents a Zulip API client
 data ZulipClient = ZulipClient { email   :: String
                                , apiKey  :: String
                                , baseUrl :: String
                                }
 
+-- |Helper for creating a `ZulipClient` with the `baseUrl` set to
+-- `defaultBaseUrl`
+newZulip :: String -> String -> ZulipClient
+newZulip e k = ZulipClient e k defaultBaseUrl
+
+-- |Represents a Zulip Message
 data Message = Message { mType   :: String
                        , content :: String
                        , to      :: [String]
@@ -20,35 +37,19 @@ data Message = Message { mType   :: String
                        , id      :: Maybe String
                        }
 
+-- |A helper for determining whether a `Message` was sent. This is the same
+-- as doing something like `Data.Maybe.isJust $ HZulip.id msg`.
 wasSent :: Message -> Bool
 wasSent (Message _ _ _ _ (Just _)) = True
 wasSent (Message _ _ _ _ Nothing)  = False
 
+-- !The default zulip API URL
 defaultBaseUrl :: String
 defaultBaseUrl = "https://api.zulip.com/v1"
 
-messagesUrl :: ZulipClient -> String
-messagesUrl = (++ "/messages") . baseUrl
-
-reqOptions :: ZulipClient -> Options
-reqOptions (ZulipClient e k _) = defaults & auth .~ basicAuth (BS.pack e) (BS.pack k)
-
-formFromMessage :: Message -> [FormParam]
-formFromMessage (Message t c r ms _) = [ "type"    := t
-                                       , "content" := c
-                                       , "to"      := r'
-                                       ] ++ case ms of
-                                                Just s -> [ "subject" := s ]
-                                                _      -> []
-  where r' = if t == "private"
-                 then show r -- JSON Arrays and Haskell Arrays should be equal
-                 else head r -- in this case
-
-newZulip :: String -> String -> Maybe String -> ZulipClient
-newZulip e k (Just u) = ZulipClient e k u
-newZulip e k Nothing  = ZulipClient e k defaultBaseUrl
-
--- POST https://api.zulip.com/v1/messages
+-- |This wraps `POST https://api.zulip.com/v1/messages` with a nicer root
+-- API. Simpler helpers for each specific case of this somewhat overloaded
+-- endpoint will also be provided in the future.
 sendMessage :: ZulipClient -> Message -> IO Message
 sendMessage z m = do
     response <- postWith (reqOptions z) (messagesUrl z) (formFromMessage m)
@@ -67,6 +68,33 @@ sendMessage z m = do
       _ -> fatalError response
   where fatalError r = error $ "The Zulip API responded with: " ++ show r
 
--- TODO:
--- POST https://api.zulip.com/v1/register
--- GET https://api.zulip.com/v1/events
+-- TODO
+-- Wrappers for:
+-- - POST https://api.zulip.com/v1/register
+-- - GET https://api.zulip.com/v1/events
+
+-- Private types and functions:
+-------------------------------------------------------------------------------
+
+-- |Gets the endpoint for creating messages for a given `ZulipClient`.
+messagesUrl :: ZulipClient -> String
+messagesUrl = (++ "/messages") . baseUrl
+
+-- |Constructs the `Wreq` HTTP request `Options` object for
+-- a `ZulipClient`.
+reqOptions :: ZulipClient -> Options
+reqOptions (ZulipClient e k _) = defaults & auth .~ basicAuth (BS.pack e) (BS.pack k)
+
+-- |Constructs a list of form-data request's `FormParam` objects from
+-- a `Message` to post to zulip's API.
+formFromMessage :: Message -> [FormParam]
+formFromMessage (Message t c r ms _) = [ "type"    := t
+                                       , "content" := c
+                                       , "to"      := r'
+                                       ] ++ case ms of
+                                                Just s -> [ "subject" := s ]
+                                                _      -> []
+  where r' = if t == "private"
+                 then show r -- JSON Arrays and Haskell Arrays should be equal
+                 else head r -- in this case
+
