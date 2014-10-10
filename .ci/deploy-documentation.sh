e07bb874c0b626b0a287828d468d1eed07d67b02
yamadapc <tacla.yamada@gmail.com>
2014-10-10 09:45:01 -0400

    Remove pipes to `/dev/null` on gh-pages deploy
    This sholuld make the haddock deployment to github pages on travis-ci a
    little more verbose, so I can debug it (it's not really working).

--- .ci/deploy-documentation.sh
+++ .ci/deploy-documentation.sh
@@ -3,17 +3,17 @@
 
 if [ "$TRAVIS_REPO_SLUG" == "yamadapc/hzulip" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Generating documentation...\n"
-  cabal haddock > /dev/null
+  cabal haddock
   echo -e "Publishing documentation...\n"
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/yamadapc/hzulip hzulip-doc > /dev/null
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/yamadapc/hzulip hzulip-doc
   cp -Rf dist/doc/html/hzulip/* $HOME/hzulip-doc/
   cd hzulip-doc
   git add -f .
   git commit -m "Latest documentation auto-deployed on $TRAVIS_BUILD_NUMBER"
-  git push -fq origin gh-pages > /dev/null
+  git push -fq origin gh-pages
 
   echo -e "Published documentation! Hack away!\n"
 fi
