f089b8e573b41aa0f97a2ebeaaf52e01082a2be8
yamadapc <tacla.yamada@gmail.com>
2014-10-10 10:13:58 -0400

    Fix travis-ci haddock deployment to gh-pages
    This fixes a typo in the `deploy-documentation.sh` script.

--- .ci/deploy-documentation.sh
+++ .ci/deploy-documentation.sh
@@ -4,13 +4,14 @@
 if [ "$TRAVIS_REPO_SLUG" == "yamadapc/hzulip" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Generating documentation...\n"
   cabal haddock
+  cp -Rf dist/doc/html/hzulip/ $HOME/hzulip-doc/
   echo -e "Publishing documentation...\n"
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
-  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/yamadapc/hzulip hzulip-doc
-  cp -Rf dist/doc/html/hzulip/* $HOME/hzulip-doc/
-  cd hzulip-doc
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/yamadapc/hzulip
+  cd hzulip
+  cp -Rf $HOME/hzulip-doc/* .
   git add -f .
   git commit -m "Latest documentation auto-deployed on $TRAVIS_BUILD_NUMBER"
   git push -fq origin gh-pages
